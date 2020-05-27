ae14d09dd01eae062d5f288d3d9031137fc2dfbf
jinyeong Lee <cookie178@naver.com>
2020-05-27 18:20:38 +0900

    Use append() for adding item

--- ChecklistV1/ContentView.swift
+++ ChecklistV1/ContentView.swift
@@ -15,10 +15,6 @@ struct ContentView: View {
             List{
                 ForEach(checklistItems, id: \.self){
                     item in Text(item)
-                        .onTapGesture {
-                            self.checklistItems.append(item)
-                            self.printChecklistContents()
-                    }
                 }
             }//end of list
             .navigationBarTitle("Checklist")
