9f16ff94adb92095d83f7d68fa8d8f26d2ded9a2
jinyeong Lee <cookie178@naver.com>
2020-05-27 17:34:40 +0900

    Initial Commit

---
+++ ChecklistV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistV1
+//
+//  Created by Release on 2020/05/27.
+//  Copyright © 2020 Jinyoung Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
