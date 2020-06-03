005667508d42cf39fe0f82736d290c57717df142
jinyeong Lee <cookie178@naver.com>
2020-06-03 18:21:35 +0900

    make a checklist.swfit for a ViewModel

--- ChecklistV1/ChecklistView.swift
+++ ChecklistV1/ChecklistView.swift
@@ -11,17 +11,13 @@ import SwiftUI
 
 
 struct ChecklistView: View {
-    @State var checklistItems = [
-        ChecklistItem(name: "Walk the dog",isChecked: false),
-        ChecklistItem(name: "Brush my teeth", isChecked: false),
-        ChecklistItem(name: "Learn iOS development", isChecked: true),
-        ChecklistItem(name: "Soccer practice", isChecked: false),
-        ChecklistItem(name: "Walk the dog", isChecked: true)
-    ]
+   
+    @ObservedObject var checklist = Checklist()
+    
     var body: some View {
         NavigationView{
             List{
-                ForEach(checklistItems){ //name in struct
+                ForEach(checklist.checklistItems){ //name in struct
                     checklistItem in
                     HStack{
                         Text(checklistItem.name)
@@ -31,41 +27,28 @@ struct ChecklistView: View {
                     .background(Color.white) //for whole ro clickable
                     .onTapGesture {//checklistitem is defined in ForEach.
                         //print("The user tapped \(checklistItem.name).") //not print in app.
-                        if let matchingIndex = self.checklistItems.firstIndex(where: { //if let : optional binding
+                        if let matchingIndex = self.checklist.checklistItems.firstIndex(where: { //if let : optional binding
                             //if not null, do it
                             $0.id == checklistItem.id }){
-                            self.checklistItems[matchingIndex].isChecked.toggle()//toggle == change
+                            self.checklist.checklistItems[matchingIndex].isChecked.toggle()//toggle == change
                         }
-                        self.printChecklistContents()
+                        self.checklist.printChecklistContents()
                     }
                 }//End of ForEach
-                .onDelete(perform: deleteListItem)
-                .onMove(perform: moveListItem)
+                    .onDelete(perform: checklist.deleteListItem)
+                    .onMove(perform: checklist.moveListItem)
 
             }//end of list
             .navigationBarItems(trailing: EditButton())
             .navigationBarTitle("Checklist")
             .onAppear(){
-                self.printChecklistContents()
+                self.checklist.printChecklistContents()
             }
         }
     }//End of body
     
 //Methond
-    func printChecklistContents(){
-        for item in checklistItems{
-            print(item)
-        }
-    }
-    
-    func deleteListItem(whichElement: IndexSet){
-        checklistItems.remove(atOffsets: whichElement)
-        printChecklistContents()
-    }
-    func moveListItem(whichElement: IndexSet, destination: Int){
-        checklistItems.move(fromOffsets: whichElement, toOffset: destination)
-        printChecklistContents()
-    }
+
 }//End of Content View
 
 struct ContentView_Previews: PreviewProvider {
